660ff647e3445fb984952ebc3cb5fc62c19f187e
Fred Ngo <fredngo@gmail.com>
2023-02-06 11:02:08 -0500

    Devolve the solution into the exercise

--- routes/eateries.js
+++ routes/eateries.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-var eateriesCtrl = require('../controllers/eateries');
+router.get('/', function(req, res, next) {
+  res.render('eateries/list');
+});
 
-router.get('/', eateriesCtrl.index);
-router.get('/:id', eateriesCtrl.show);
+router.get('/3226', function(req, res, next) {
+  res.render('eateries/3226');
+});
+
+router.get('/5475', function(req, res, next) {
+  res.render('eateries/5475');
+});
+
+router.get('/9567', function(req, res, next) {
+  res.render('eateries/9567');
+});
 
 module.exports = router;
